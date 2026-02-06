bcae93cef12d1ae1ea1ff5d822ed6e1521c90cfe
phanhxichlo <mthg888@gmail.com>
2026-02-06 23:30:42 +0700

    Update main.py

--- thmaka/backend/main.py
+++ thmaka/backend/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 from api.grading import router as grading_router
 
-app = FastAPI(title="Web Chấm Trắc Nghiệm")
+app = FastAPI()
 
 app.include_router(grading_router)
-
-# chạy:
-# uvicorn main:app --reload
