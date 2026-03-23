c3ee3ebae06abf13b736dc91638e41f68fe7373c
phanhxichlo <mthg888@gmail.com>
2026-03-23 22:59:20 +0700

    Delete thmaka directory

--- thmaka/backend/config.py
+++
@@ -1,3 +0,0 @@
-UPLOAD_DIR = "uploads"
-NUM_QUESTIONS = 40
-CHOICES = ["A", "B", "C", "D"]
--- thmaka/backend/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from api.grading import router as grading_router
-
-app = FastAPI()
-
-app.include_router(grading_router)
